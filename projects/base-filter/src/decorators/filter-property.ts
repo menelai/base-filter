3d9f0e946ddfcedb52920d5f6337db489975e7c5
kovalenko <kovalenko@gmail.com>
2026-05-29 15:51:15 +0300

    fix: filterProperties

--- projects/base-filter/src/decorators/filter-property.ts
+++ projects/base-filter/src/decorators/filter-property.ts
@@ -1,6 +1,7 @@
 import {SerializeFn} from '../serialize-fn';
 
 import {BaseSignalFilter} from '../base-signal-filter';
+import {transformFn} from '@kovalenko/base-filter/src';
 
 /**
  * Defines filter property
@@ -9,15 +10,12 @@ import {BaseSignalFilter} from '../base-signal-filter';
  */
 export function FilterProperty(serialize?: SerializeFn): PropertyDecorator {
   return (object: any, propertyName: string | symbol): void => {
-    const c = object.constructor as typeof BaseSignalFilter;
+    const ctor = transformFn(object, propertyName, v => v);
+
     const p = propertyName as keyof BaseSignalFilter;
 
-    if (!c.filterProperties.has(p)) {
-      c.filterProperties.set(p, []);
-    }
-
     if (serialize) {
-      c.serializeField.set(p, serialize);
+      ctor.serializeField.set(p, serialize);
     }
   };
 }
--- projects/base-filter/src/decorators/transform-fn.ts
+++ projects/base-filter/src/decorators/transform-fn.ts
@@ -1,12 +1,20 @@
 import {BaseSignalFilter} from '../base-signal-filter';
 
-export function transformFn(object: any, propertyName: string | symbol, parser: (v: unknown) => any): void {
+export function transformFn(object: any, propertyName: string | symbol, parser: (v: unknown) => any): typeof BaseSignalFilter {
   const ctor = object.constructor as typeof BaseSignalFilter;
 
   if (!Object.prototype.hasOwnProperty.call(ctor, 'filterProperties')) {
     const parentProps = (ctor as any).filterProperties ?? [];
     Object.defineProperty(ctor, 'filterProperties', {
       value: new Map(parentProps),
+      writable: true,
+    });
+  }
+
+  if (!Object.prototype.hasOwnProperty.call(ctor, 'serializeField')) {
+    const parentSerialize = (ctor as any).serializeField ?? [];
+    Object.defineProperty(ctor, 'serializeField', {
+      value: new Map(parentSerialize),
       writable: true,
     });
   }
@@ -20,4 +28,6 @@ export function transformFn(object: any, propertyName: string | symbol, parser: 
   }
 
   parsers.push(parser);
+
+  return ctor;
 }
